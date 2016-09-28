baeb48109a37e83dbbabd56fac33a2012fd735df
mclavelguzman <mclavelguzman@gmail.com>
2016-09-28 11:01:39 -0600

    add image, fix text to space

--- www/js/app.js
+++ www/js/app.js
@@ -1,6 +1,6 @@
 (function() {
 
-var app = angular.module('starter', ['ionic'])
+var app = angular.module('myreddit', ['ionic']);
 
 app.controller('RedditCtrl', function($http, $scope) {
 
@@ -9,20 +9,21 @@ app.controller('RedditCtrl', function($http, $scope) {
   $http.get('https://www.reddit.com/r/Android/new/.json')
     .success(function(response) {
       angular.forEach(response.data.children, function(child) {
-        $scope.stories.push(child.data)
+        $scope.stories.push(child.data);
       });
     });
+
 });
 
 app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      // cordova.plugins.Keyboard.disableScroll(true);
     }
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
   });
- });
+});
+
 }());
